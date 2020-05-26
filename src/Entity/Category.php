f232db7ba67edad61ee77b4b96871ffb9d9eb29a
Aktar Zaman <aktarzaman@berkeley.edu>
2020-05-26 14:41:36 -0400

    added a few methods about the type of functions for setter and getters for different data types I'll be dealing with

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -12,6 +12,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 class Category {
 
+
   /**
      * @var int
      *
@@ -42,4 +43,112 @@ class Category {
      * @ORM\ManyToMany(targetEntity="Affiliate", mappedBy="categories")
      */
     private $affiliates;
+
+    public function __construct()
+    {
+      $this -> jobs = new ArrayCollection();
+      $this -> affiliates = new ArrayCollection();
+    }
+
+
+<!-- Setting up setter and getter based on what kind of data I am dealing with -->
+    /**
+    * @return int
+    */
+    public function getId() : ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName() : ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return self
+     */
+    public function setName(string $name) : self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Job[]|ArrayCollection
+     */
+    public function getJobs()
+    {
+        return $this->jobs;
+    }
+
+    /**
+     * @param Job $job
+     *
+     * @return self
+     */
+    public function addJob(Job $job) : self
+    {
+        if (!$this->jobs->contains($job)) {
+            $this->jobs->add($job);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param Job $job
+     *
+     * @return self
+     */
+    public function removeJob(Job $job) : self
+    {
+        $this->jobs->removeElement($job);
+
+        return $this;
+    }
+
+    /**
+     * @return Affiliate[]|ArrayCollection
+     */
+    public function getAffiliates()
+    {
+        return $this->affiliates;
+    }
+
+    /**
+     * @param Affiliate $affiliate
+     *
+     * @return self
+     */
+    public function addAffiliate($affiliate) : self
+    {
+        if (!$this->affiliates->contains($affiliate)) {
+            $this->affiliates->add($affiliate);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param Affiliate $affiliate
+     *
+     * @return self
+     */
+    public function removeAffiliate($affiliate) : self
+    {
+        $this->affiliates->removeElement($affiliate);
+
+        return $this;
+    }
+
+
+
+
 }
